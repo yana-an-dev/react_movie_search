c17ba4f4970e5aeda4b20163a125deb0a3ed484c
Yana <ayh916@gmail.com>
2021-08-05 19:09:50 +1000

    new component: MoiveCard.js

--- my-app/src/SearchMovies.js
+++ my-app/src/SearchMovies.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react"
+import MovieCard from './MovieCard'
+
 export default function SearchMovies() {
 
     //states - input query, movies
@@ -37,22 +39,7 @@ export default function SearchMovies() {
 
             <div className="card-list">
                 {movies.filter(movie => movie.poster_path).map(movie => (
-                    <div key={movie.id} className="card">
-                        <img className="card--image"
-                            src={`https://www.themoviedb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-
-                            //  `https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}
-
-                            alt={movie.title + 'poster'} />
-
-                        <div className="card--content">
-                            <h3 className="card--title">{movie.title}</h3>
-                            <p><small>Release data: {movie.release_date}</small></p>
-                            <p><small>Rating: {movie.vote_average}</small></p>
-                            <p className="card--desc">{movie.overview}</p>
-                        </div>
-
-                    </div>
+                    <MovieCard key={movie.id} movie={movie} />
                 ))}
 
             </div>
